248760be65a7fb721aaa402d2df37a43092b7af5
YuukiHaraProgramming <hara@nlp.ist.i.kyoto-u.ac.jp>
2023-12-30 23:52:21 +0900

    add the choice of BinaryClassifierByTARGET

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,13 +1,30 @@
 import os
 import torch
 from finetune.model_pred_by_cls import BinaryClassifierByCLS
+from finetune.model_pred_by_target import BinaryClassifierByTARGET
 from datasets import load_dataset
 from tqdm import tqdm
 from argparse import ArgumentParser
+from typing import Literal
+
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+from transformers import default_data_collator
 
 class DatasetEvaluation:
-    def __init__(self, model_name, checkpoint_path, mode) -> None:
-        self.finetuned_model = BinaryClassifierByCLS.load_from_checkpoint(checkpoint_path, model_name=model_name, add_target_token=False if mode == 'base' else True)
+    def __init__(
+            self,
+            model_name: str,
+            checkpoint_path: str,
+            mode: Literal['base', 'pre_target_timeline', 'all_timeline'],
+            pred_by: Literal['cls', 'target'],
+            concat_or_mean: Literal['concat', 'mean']
+            ) -> None:
+        if pred_by == 'cls':
+            self.finetuned_model = BinaryClassifierByCLS.load_from_checkpoint(checkpoint_path, model_name=model_name, add_target_token=False if mode == 'base' else True)
+        elif pred_by == 'target':
+            self.finetuned_model = BinaryClassifierByTARGET.load_from_checkpoint(checkpoint_path, model_name=model_name, concat_or_mean=concat_or_mean)
+            self.finetuned_model.set_concat_or_mean(concat_or_mean)
         self.model = self.finetuned_model.model
         self.tokenizer = self.finetuned_model.tokenizer
 
@@ -34,9 +51,16 @@ class DatasetEvaluation:
 
                 input_ids = tokenized_src_texts['input_ids'].to(self.model.device)
                 attention_mask = tokenized_src_texts['attention_mask'].to(self.model.device)
-                output = self.model(input_ids=input_ids, attention_mask=attention_mask)
-                proba = self.finetuned_model.sigmoid(output.logits).squeeze()
-                pred = proba.item()
+                if self.finetuned_model.__class__.__name__ == 'BinaryClassifierByCLS':
+                    output = self.model(input_ids=input_ids, attention_mask=attention_mask)
+                    proba = self.finetuned_model.sigmoid(output.logits).squeeze()
+                    # proba = self.finetuned_model.forward(input_ids=input_ids, attention_mask=attention_mask)
+                    pred = proba.item()
+                elif self.finetuned_model.__class__.__name__ == 'BinaryClassifierByTARGET':
+                    loggits = self.finetuned_model.forward(input_ids=input_ids, attention_mask=attention_mask)
+                    sigmoid = torch.nn.Sigmoid()
+                    preba = sigmoid(loggits).squeeze()
+                    pred = preba.item()
 
                 if tgt_texts == 1:
                     if pred >= 0.5:
@@ -58,6 +82,8 @@ class DatasetEvaluation:
 
 def main():
     parser = ArgumentParser()
+    parser.add_argument('--pred_by', default='target', choices=['cls', 'target'])
+    parser.add_argument('--concat_or_mean', default='concat', choices=['concat', 'mean'])
     parser.add_argument('--mode', default=None, choices=['base', 'pre_target_timeline', 'all_timeline'])
     parser.add_argument("--root_dir", default='/mnt/mint/hara/datasets/news_category_dataset/dataset')
     parser.add_argument('--sub_dir', default='', help='e.g., diff7_rep1, diff7_rep3, diff7_ins1, diff6_rep1, diff6_rep3, diff6_ins1')
@@ -69,7 +95,7 @@ def main():
     test_data_dir = os.path.join(args.root_dir, args.sub_dir, args.mode)
     print(checkpoint_path)
 
-    de = DatasetEvaluation(args.model_name, checkpoint_path, args.mode)
+    de = DatasetEvaluation(args.model_name, checkpoint_path, args.mode, args.pred_by, args.concat_or_mean)
     de.evaluate(test_data_dir)
 
 if __name__ == '__main__':
